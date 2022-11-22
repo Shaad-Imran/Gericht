6ed89e1c2f72a67ec8b2739f72a9c7237d177320
Imran <imranmansuri038@gmail.com>
2022-11-22 15:43:33 +0530

    commit init

---
+++ src/components/Navbar/NavData.js
@@ -0,0 +1,23 @@
+export const Data = [
+  {
+    link: "#home",
+    Navmenu: "Home",
+  },
+
+  {
+    link: "about",
+    Navmenu: "About",
+  },
+  {
+    link: "#menu",
+    Navmenu: "Menu",
+  },
+  {
+    link: "#awards",
+    Navmenu: "Awards",
+  },
+  {
+    link: "#contact",
+    Navmenu: "Contact",
+  },
+];
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+import images from "../../constants/images";
+import { Data } from "./NavData";
+
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="gericht-logo" />
+      </div>
+
+      <ul className="app__navbar-links">
+        {Data.map((value) => {
+          return (
+            <li className="p__opensans">
+              <a href={value.link}>{value.Navmenu}</a>
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Log In / Registration
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book Table
+        </a>
+      </div>
+
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app__navbar-smallscreen_links">
+              {Data.map((value) => {
+                return (
+                  <li>
+                    <a href={value.link} onClick={() => setToggleMenu(false)}>
+                      {value.Navmenu}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
